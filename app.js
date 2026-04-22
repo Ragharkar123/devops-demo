c8f11dd38636321fd6cdc4438ded0f4f5f9fe8c8
Rohit_Agharkar <rohitagharkar@benchmarkit.solutions>
2026-04-22 09:47:15 +0000

    Changes in app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from DevOps Training");
-});
+  res.end("Hello from DevOps Training")
+})
 
-server.listen(3000, () => {
+server.listen(3000, ) => {
   console.log("Server running on port 3000");
 });
